d2bb3c6a44b0847caae8ce76880cd96e9ebbdab2
vityajulin <vkdvornokov@gmail.com>
2020-06-04 16:47:43 +0300

    edit text pin

--- app/src/main/java/com/example/locknote/LoginActivity.java
+++ app/src/main/java/com/example/locknote/LoginActivity.java
@@ -3,12 +3,20 @@ package com.example.locknote;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
     private int progress = 0;
@@ -26,6 +34,42 @@ public class LoginActivity extends AppCompatActivity {
         Button delete = findViewById(R.id.btn_delete);
         final ProgressBar progressBar = findViewById(R.id.bar_progress);
 
+        final EditText edTxt = findViewById(R.id.edTxt_pinCode);
+
+        /*edTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                progress += 1;
+                progressBar.setProgress(progress);
+                if (progress == 4) {
+                    finish();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });*/
+
+        edTxt.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == 66) {
+                    progress += 1;
+                    progressBar.setProgress(progress);
+                    return true;
+                }
+                if (keyCode == 4) {
+                    progress -= 1;
+                    progressBar.setProgress(progress);
+                }
+                return false;
+            }
+        });
 
         num1.setOnClickListener(new View.OnClickListener() {
             @Override
